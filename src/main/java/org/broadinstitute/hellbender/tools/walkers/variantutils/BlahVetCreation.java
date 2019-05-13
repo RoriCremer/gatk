f4dd53137b584f400df8dc0a2fd275d712107f5e
rori <aurora@wm41b-664.broadinstitute.org>
2019-05-13 17:59:59 -0400

    update vet schema

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/BlahVetCreation.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/BlahVetCreation.java
@@ -1,40 +1,52 @@
 package org.broadinstitute.hellbender.tools.walkers.variantutils;
-
-import com.fasterxml.jackson.annotation.JsonValue;
+import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.VariantContext;
-import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
-import org.broadinstitute.hellbender.cmdline.programgroups.ExampleProgramGroup;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@CommandLineProgramProperties(
-        summary = "Prints variants supplied to the specified output file (stdout if none provided), along with overlapping reads/reference bases/variants (if provided)",
-        oneLineSummary = "Prints variants with optional contextual data",
-        programGroup = ExampleProgramGroup.class,
-        omitFromCommandLine = true
-)
 public final class BlahVetCreation {
+
+    private static final String DELIMITER = ",";
 
     /**
      * Expected headers for the Variant Table (VET)
+     * start_position, // req
+     * reference_bases, // req
+     * alternate_bases, // req ** concat all alt bases with '|' delimiter
+     * alternate_bases.alt, // req
+     * alternate_bases.AS_RAW_MQ, // req
+     * alternate_bases.AS_MQ_DP, // req
+     * alternate_bases.AS_RAW_MQRankSum,
+     * alternate_bases.AS_QUALapprox, // req
+     * alternate_bases.AS_RAW_ReadPosRankSum,
+     * alternate_bases.AS_SB_TABLE, // req
+     * alternate_bases.AS_VarDP, // req
+     * call, // req **
+     * call_name, // req
+     * call_genotype, // req
+     * call_AD,
+     * call_DP,
+     * call_GQ, // req
+     * call_PGT,
+     * call_PID,
+     * call_PL
      */
     public enum HeaderFieldEnum {
         // TODO is this where the validation step (required vs not) lives  -- fail if there is missing data for a required field
         // and just leave it empty if not required
 
-        START_POSITION("start_position") { // Required
+        START_POSITION { // Required
              public String getColumnValue(final VariantContext variant) throws IOException {
                  final String startPosition = String.valueOf(variant.getStart());
-                 if (startPosition.equals(null)) {
-                     throw new IllegalArgumentException("Cannot be missing required value for start_position");
-                 }
                  return startPosition; // TODO see what James' writer does
             }
         },
 
-        REFERENCE_BASES("reference_bases") { // Required
+        REFERENCE_BASES { // Required
             public String getColumnValue(final VariantContext variant) throws IOException {
                 final String referenceBase = variant.getReference().getBaseString();
                 if (referenceBase.equals(null)) {
@@ -44,153 +56,135 @@ public final class BlahVetCreation {
             }
         },
 
-        ALTERNATE_BASES_ALT("alternate_bases.alt") {
+        ALTERNATE_BASES_ALT {
             public String getColumnValue(final VariantContext variant) {
-                return variant.getAlternateAllele(0).getBaseString();
+                String output = "";
+                for(Allele a : variant.getAlternateAlleles()) {
+                    output += a.getBaseString() + "|";
+                }
+                return output;
             }
         },
 
-        ALTERNATE_BASES_AS_RAW_MQ("alternate_bases.AS_RAW_MQ") { // Required
+        ALTERNATE_BASES_AS_RAW_MQ { // Required
             public String getColumnValue(final VariantContext variant) throws IOException {
-                if (variant.getAttribute("AS_RAW_MQ").equals(null)) {
+                String out = variant.getAttributeAsString("AS_RAW_MQ", null);
+                if (out.equals(null)) {
                     throw new IllegalArgumentException("Cannot be missing required value for alternate_bases.AS_RAW_MQ");
                 }
-                return variant.getAttributeAsString("AS_RAW_MQ", null);
+                return out;
             }
         },
 
-        ALTERNATE_BASES_AS_MQ_DP("alternate_bases.AS_MQ_DP") { // Required // TODO check for what this should be
+        ALTERNATE_BASES_AS_MQ_DP { // Required // TODO check for what this should be
             public String getColumnValue(final VariantContext variant) throws IOException {
-                if (variant.getAttribute("AS_MQ_DP").equals(null)) {
+                String out = variant.getAttributeAsString("AS_MQ_DP", null);
+                if (out.equals(null)) {
                     throw new IllegalArgumentException("Cannot be missing required value for alternate_bases.AS_MQ_DP");
                 }
-                return variant.getAttributeAsString("AS_MQ_DP", null);
+                return out;
             }
         },
 
-        ALTERNATE_BASES_AS_RAW_MQRankSum("alternate_bases.AS_RAW_MQRankSum") {
+        ALTERNATE_BASES_AS_RAW_MQRANKSUM {
             public String getColumnValue(final VariantContext variant) {
                 return variant.getAttributeAsString("AS_RAW_MQRankSum", null);
             }
         },
 
-        ALTERNATE_BASES_AS_QUAL_APPROX("alternate_bases.AS_QUALapprox") { // Required
+        ALTERNATE_BASES_AS_QUALAPPROX { // Required
             public String getColumnValue(final VariantContext variant) throws IOException {
-                if (variant.getAttribute("AS_QUALapprox").equals(null)) {
+                String out = variant.getAttributeAsString("AS_QUALapprox", null);
+                if (out.equals(null)) {
                     throw new IllegalArgumentException("Cannot be missing required value for alternate_bases.AS_QUALapprox");
                 }
-                return variant.getAttributeAsString("AS_QUALapprox", null);
+                return out;
             }
         },
 
-        ALTERNATE_BASES_AS_RAW_READPOSRANKSUM("alternate_bases.AS_RAW_ReadPosRankSum") {
+        ALTERNATE_BASES_AS_RAW_READPOSRANKSUM {
             public String getColumnValue(final VariantContext variant) {
                 return variant.getAttributeAsString("AS_RAW_ReadPosRankSum", null);
             }
         },
 
-        ALTERNATE_BASES_AS_SB_TABLE("alternate_bases.AS_SB_TABLE") { // Required
+        ALTERNATE_BASES_AS_SB_TABLE { // Required
             public String getColumnValue(final VariantContext variant) throws IOException {
-                if (variant.getAttribute("AS_SB_TABLE").equals(null)) {
+                String out = variant.getAttributeAsString("AS_SB_TABLE", null);
+                if (out.equals(null)) {
                     throw new IllegalArgumentException("Cannot be missing required value for alternate_bases.AS_SB_TABLE");
                 }
-                return variant.getAttributeAsString("AS_SB_TABLE", null);
+                return out;
             }
         },
 
-        ALTERNATE_BASES_AS_VARDP("alternate_bases.AS_VarDP") { // Required
+        ALTERNATE_BASES_AS_VARDP { // Required
             public String getColumnValue(final VariantContext variant) throws IOException {
-                if (variant.getAttribute("AS_VarDP").equals(null)) {
+                String out = variant.getAttributeAsString("AS_QUALapprox", null);
+                if (out.equals(null)) {
                     throw new IllegalArgumentException("Cannot be missing required value for alternate_bases.AS_VarDP");
                 }
-                return variant.getAttributeAsString("AS_VarDP", null);
+                return out;
             }
         },
 
-        /*FILTER("filter") {
-            public Set<String> getColumnValue(final VariantContext variant) {
-                return variant.getFilters();
-            }
-        },*/
-
-        CALL_NAME("call.name") {
+        CALL_NAME {
             public String getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).getGenotypeString(); // TODO double check
+                return variant.getGenotype(0).getSampleName();
             }
         },
 
-        CALL_GENOTYPE("call.genotype") {
+        CALL_GENOTYPE {
             public String getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).getGenotypeString(); // TODO double check
+                return variant.getGenotype(0).getGenotypeString();
             }
         },
 
-         /*CALL_PHASESET("call.phaseset") {
-            public int[] getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).get .....// TODO lookup
-            }
-        },
-
-        CALL_AD("call.AD") {
-            public Object getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).getAD();
-            }
-        },
-
-        CALL_DP("call.DP") {
-            public Object getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).getDP();
-            }
-        },
-
-        CALL_GQ("call.GQ") { // Required
-            if (variant.getGenotype(0).getGQ().equals(null)) {
-               throw new IllegalArgumentException("Cannot be missing required value for call.GQ");
-            }
-            public Object getColumnValue(final VariantContext variant) throws IOException {
-                return variant.getGenotype(0).getGQ();
-            }
-        },
-
-        CALL_PGT("call.PGT") {
-            public Object getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).getAnyAttribute("PGT");// TODO lookup -- should be in Format -- cast to int
-            }
-        },
-
-        CALL_PID("call.PID") {
-            public Object getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).getAnyAttribute("PID");// TODO lookup -- should be in Format
-            }
-        },*/
-
-
-        CALL_PL("call.PL") { // TODO make this a string -- Need to check in with Laura about how we want to concat the string together
+        CALL_AD {
             public String getColumnValue(final VariantContext variant) {
-                return variant.getGenotype(0).getPL().toString();
+                return Arrays.stream(variant.getGenotype(0).getAD())
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(DELIMITER));
             }
-        /*},
+        },
 
-        AS_SB_TABLE("AS_SB_TABLE") {
+        CALL_DP {
             public String getColumnValue(final VariantContext variant) {
-                return variant.get .....// TODO lookup
-            }*/
+                return String.valueOf(variant.getGenotype(0).getDP());
+            }
+        },
+
+        CALL_GQ { // Required
+            public String getColumnValue(final VariantContext variant) throws IOException {
+                if (!variant.getGenotype(0).hasGQ()) {
+                    throw new IllegalArgumentException("Cannot be missing required value for call.GQ");
+                }
+                return  String.valueOf(variant.getGenotype(0).getGQ());
+            }
+        },
+
+        CALL_PGT {
+            public String getColumnValue(final VariantContext variant) {
+                return String.valueOf(variant.getGenotype(0).getAnyAttribute("PGT"));
+            }
+        },
+
+        CALL_PID {
+            public String getColumnValue(final VariantContext variant) {
+                return String.valueOf(variant.getGenotype(0).getAnyAttribute("PID")); // TODO check in with Laura about PID
+            }
+        },
+
+        CALL_PL {
+            public String getColumnValue(final VariantContext variant) {
+                return Arrays.stream(variant.getGenotype(0).getPL())
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(DELIMITER));
+            }
         };
-
-        private String value;
-
-        HeaderFieldEnum(String value) {
-            this.value = value;
-        }
 
         public String getColumnValue(final VariantContext variant) throws IOException {
             throw new IllegalArgumentException("Not implemented");
-        }
-
-        @Override
-        @JsonValue
-        public String toString() {
-            return String.valueOf(value);
         }
     }
 
@@ -202,5 +196,9 @@ public final class BlahVetCreation {
             row.add(fieldEnum.getColumnValue(variant));
         }
         return row;
+    }
+
+    public static List<String> getHeaders() {
+        return Arrays.stream(HeaderFieldEnum.values()).map(String::valueOf).collect(Collectors.toList());
     }
 }
