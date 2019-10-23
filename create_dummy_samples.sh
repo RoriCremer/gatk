6d0474ef4846c6c3425fee51efcae382d3f0b541
rori <6863459+RoriCremer@users.noreply.github.com>
2019-10-23 12:40:35 -0400

    param updates

--- create_dummy_samples.sh
+++ create_dummy_samples.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bas
 
 
-COUNTER=0
-       while [  $COUNTER -lt 24 ]; do
-           echo $COUNTER
-           bq query  --allow_large_results --destination_table=joint_genotyping_chr20_dummy_data_scale_testing.vet  --append_table --project_id=broad-dsp-spec-ops "SELECT position, ref, alt, AS_RAW_MQ, AS_RAW_MQRankSum, AS_QUALapprox, AS_RAW_ReadPosRankSum, AS_SB_TABLE, AS_VarDP, CONCAT(sample, '_d') as sample, call_GT, call_AD, call_DP, call_GQ, call_PGT, call_PID, call_PL FROM `broad-dsp-spec-ops.joint_genotyping_chr20_dalio_40000_july_updated.vet"
-           let COUNTER=COUNTER+1
+PROJECT_ID="spec-ops-sand-box"
+#"broad-dsp-spec-ops"
+ORIGIN_DATASET="chr20"
+DESTINATION_DATASET="chr20_dummy_million"
+
+
+VET_COUNTER=0
+       while [  $VET_COUNTER -lt 24 ]; do
+           echo $VET_COUNTER
+           bq query  --allow_large_results --destination_table=$DESTINATION_DATASET.vet  --append_table --project_id=$PROJECT_ID \
+           "SELECT position, ref, alt, AS_RAW_MQ, AS_RAW_MQRankSum, AS_QUALapprox, AS_RAW_ReadPosRankSum, AS_SB_TABLE, AS_VarDP, CONCAT(sample, '_d') as sample, call_GT, call_AD, call_DP, call_GQ, call_PGT, call_PID, call_PL FROM $ORIGIN_DATASET.vet"
+           let VET_COUNTER=VET_COUNTER+1
+       done
+
+
+PET_COUNTER=0
+       while [  $PET_COUNTER -lt 24 ]; do
+           echo $PET_COUNTER
+           bq query  --allow_large_results --destination_table=$DESTINATION_DATASET.pet --append_table --project_id=$PROJECT_ID \
+           "SELECT position, CONCAT(sample, '_d$COUNTER') as sample, state FROM $ORIGIN_DATASET.pet"
+           let PET_COUNTER=PET_COUNTER+1
        done
 
 
 COUNTER=0
        while [  $COUNTER -lt 24 ]; do
            echo $COUNTER
-           bq query  --allow_large_results --destination_table=joint_genotyping_chr20_dummy_data_scale_testing.pet_without_gq60 --append_table --project_id=broad-dsp-spec-ops "SELECT position, CONCAT(sample, '_d$COUNTER') as sample, state FROM joint_genotyping_chr20_dalio_40000_july_updated.pet_without_gq60"
+           bq query  --allow_large_results --destination_table=$DESTINATION_DATASET.sample_list  --append_table  --project_id=$PROJECT_ID \
+           "SELECT CONCAT(sample, '_d$COUNTER') as sample FROM metadata.sample_list"
            let COUNTER=COUNTER+1
        done
-
-
-COUNTER=0
-       while [  $COUNTER -lt 24 ]; do
-           echo $COUNTER
-           bq query  --allow_large_results --destination_table=joint_genotyping_chr20_dummy_data_scale_testing.sample_list  --append_table  --project_id=broad-dsp-spec-ops "SELECT CONCAT(sample, '_d$COUNTER') as sample FROM joint_genotyping_chr20_dalio_40000_july_updated.sample_list"
-           let COUNTER=COUNTER+1
-       done
-
-
-
-
